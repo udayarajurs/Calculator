6d73ef890c8baaa2e1b59e452ca119ad7d163cb9
Udaya raj urs <udayarajurs7@gmail.com>
2024-07-20 17:56:50 +0530

    Change desging

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,6 +6,10 @@ import {
   View,
   Image,
   Dimensions,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+  ToastAndroid,
 } from 'react-native';
 import Button from './src/components/Button';
 import Row from './src/components/Row';
@@ -23,13 +27,47 @@ const App = () => {
     setState(state => calculator(type, value, state));
   };
 
+  const [firstNumber, setFirstNumber] = useState(null);
+  const [secondNumber, setSecondNumber] = useState(null);
+  const [result, setResult] = useState(null);
+  const [funaction, setFunaction] = useState(null);
+
+  const calculate = (one, two, funact) => {
+    let data = null;
+    const num1 = Number(one);
+    const num2 = Number(two);
+
+    switch (funact) {
+      case 'Add':
+        data = num1 + num2;
+        break;
+      case 'Subtract':
+        data = num1 - num2;
+        break;
+      case 'Multiply':
+        data = num1 * num2;
+        break;
+      case 'Divide':
+        if (num2 !== 0) {
+          data = num1 / num2;
+        } else {
+          data = 'Error: Division by zero';
+        }
+        break;
+      default:
+        data = 'Error: Invalid operation';
+    }
+
+    setResult(data);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       {/* Status bar here */}
 
       <View
         style={{
-          flex: 0.2,
+          flex: 0.1,
           flexDirection: 'row',
           justifyContent: 'space-between',
         }}>
@@ -54,13 +92,208 @@ const App = () => {
         />
       </View>
 
-      <View style={{flex: 0.8, justifyContent: 'flex-end'}}>
-        <Text style={styles.value}>
+      <ScrollView keyboardShouldPersistTaps={'handled'} style={{flex: 0.9}}>
+        <View
+          style={{
+            alignItems: 'center',
+          }}>
+          <TextInput
+            style={{
+              marginTop: 50,
+              borderWidth: 1,
+              borderColor: '#000',
+              paddingHorizontal: WIDTH * 0.22,
+              borderRadius: 10,
+            }}
+            keyboardType="numeric"
+            textAlign="center"
+            placeholder="Please Enter First Operand"
+            value={firstNumber}
+            onChangeText={data => setFirstNumber(data)}
+          />
+          <TextInput
+            style={{
+              marginTop: 30,
+              borderWidth: 1,
+              borderColor: '#000',
+              paddingHorizontal: WIDTH * 0.2,
+              borderRadius: 10,
+            }}
+            value={secondNumber}
+            textAlign="center"
+            keyboardType="numeric"
+            placeholder="Please Enter Second Operand"
+            onChangeText={data => setSecondNumber(data)}
+          />
+        </View>
+
+        <View style={{alignItems: 'center', marginVertical: 30}}>
+          <Text style={{color: '#000'}}>
+            {result === null ? '' : `Your Result is : ${result}`}
+          </Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+          }}>
+          <TouchableOpacity
+            onPress={() => {
+              firstNumber === null ||
+              secondNumber === null ||
+              firstNumber === '' ||
+              secondNumber === ''
+                ? ToastAndroid.show(
+                    'Please Enter First and Second operand',
+                    ToastAndroid.LONG,
+                  )
+                : setFunaction('Add');
+            }}>
+            <Text
+              style={{
+                color: funaction === 'Add' ? '#000' : '#fff',
+                backgroundColor: funaction === 'Add' ? '#E8FFDB' : '#7B82EA',
+                fontWeight: 'bold',
+                paddingHorizontal: WIDTH * 0.15,
+                paddingVertical: 8,
+                borderRadius: 5,
+              }}>{`Add ( + )`}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              firstNumber === null ||
+              secondNumber === null ||
+              firstNumber === '' ||
+              secondNumber === ''
+                ? ToastAndroid.show(
+                    'Please Enter First and Second operand',
+                    ToastAndroid.LONG,
+                  )
+                : setFunaction('Subtract');
+            }}>
+            <Text
+              style={{
+                color: funaction === 'Subtract' ? '#000' : '#fff',
+                backgroundColor:
+                  funaction === 'Subtract' ? '#E8FFDB' : '#7B82EA',
+                fontWeight: 'bold',
+                paddingHorizontal: WIDTH * 0.1,
+                paddingVertical: 8,
+                borderRadius: 5,
+              }}>{`Subtract ( - )`}</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginVertical: 25,
+          }}>
+          <TouchableOpacity
+            onPress={() => {
+              firstNumber === null ||
+              secondNumber === null ||
+              firstNumber === '' ||
+              secondNumber === ''
+                ? ToastAndroid.show(
+                    'Please Enter First and Second operand',
+                    ToastAndroid.LONG,
+                  )
+                : setFunaction('Multiply');
+            }}>
+            <Text
+              style={{
+                color: funaction === 'Multiply' ? '#000' : '#fff',
+                backgroundColor:
+                  funaction === 'Multiply' ? '#E8FFDB' : '#7B82EA',
+                fontWeight: 'bold',
+                paddingHorizontal: WIDTH * 0.12,
+                paddingVertical: 8,
+                borderRadius: 5,
+              }}>{`Multiply ( * )`}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              firstNumber === null ||
+              secondNumber === null ||
+              firstNumber === '' ||
+              secondNumber === ''
+                ? ToastAndroid.show(
+                    'Please Enter First and Second operand',
+                    ToastAndroid.LONG,
+                  )
+                : setFunaction('Divide');
+            }}>
+            <Text
+              style={{
+                color: funaction === 'Divide' ? '#000' : '#fff',
+                backgroundColor: funaction === 'Divide' ? '#E8FFDB' : '#7B82EA',
+                fontWeight: 'bold',
+                paddingHorizontal: WIDTH * 0.12,
+                paddingVertical: 8,
+                borderRadius: 5,
+              }}>{`Divide ( / )`}</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginVertical: 25,
+          }}>
+          {funaction === null ||
+          firstNumber === null ||
+          secondNumber === null ||
+          firstNumber === '' ||
+          secondNumber === '' ? (
+            <TouchableOpacity
+              onPress={() =>
+                ToastAndroid.show(
+                  'Please Enter First and Second operand and select Operand',
+                  ToastAndroid.LONG,
+                )
+              }>
+              <Text
+                style={{
+                  color: '#fff',
+                  backgroundColor: '#A2A2A2',
+                  fontWeight: 'bold',
+                  paddingHorizontal: WIDTH * 0.2,
+                  paddingVertical: 8,
+                  borderRadius: 5,
+                }}>
+                Submit
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              onPress={() => {
+                calculate(firstNumber, secondNumber, funaction);
+              }}>
+              <Text
+                style={{
+                  color: '#fff',
+                  backgroundColor: '#00A3FF',
+                  fontWeight: 'bold',
+                  paddingHorizontal: WIDTH * 0.2,
+                  paddingVertical: 8,
+                  borderRadius: 5,
+                }}>
+                Submit
+              </Text>
+            </TouchableOpacity>
+          )}
+        </View>
+
+        {/* <Text style={styles.value}>
           {state?.expression === state.displayValue ? '' : state?.expression}
         </Text>
 
         <Text style={styles.value}>{state.displayValue}</Text>
-        {/* Create component Row */}
+        {/* Create component Row
         <Row>
           <Button
             text="C"
@@ -86,7 +319,7 @@ const App = () => {
             onPress={() => handleTap('operator', '/')}
           />
         </Row>
-        {/* Number */}
+ 
         <Row>
           <Button text="7" onPress={() => handleTap('number', 7)} />
           <Button text="8" onPress={() => handleTap('number', 8)} />
@@ -121,8 +354,8 @@ const App = () => {
           <Button text="0" onPress={() => handleTap('number', 0)} />
           <Button text="." onPress={() => handleTap('number', '.')} />
           <Button text="=" theme="primary" onPress={() => handleTap('equal')} />
-        </Row>
-      </View>
+        </Row> */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -131,7 +364,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#202020',
+    backgroundColor: '#FFF',
     justifyContent: 'flex-end',
   },
   value: {
